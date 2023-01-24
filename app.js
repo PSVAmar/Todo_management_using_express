feb13f3f47b6bd965db06e689210221238887e41
PSVAmar <20pa1a05d4@vishnu.edu.in>
2023-01-24 16:15:37 +0530

    app

---
+++ app.js
@@ -0,0 +1,66 @@
+/* eslint-disable no-unused-vars */
+const express = require("express");
+const app = express();
+const { Todo } = require("./models");
+const bodyparser = require("body-parser");
+//const { json } = require("sequelize");
+app.use(bodyparser.json());
+// app.get("/",(request,response)=>{
+//     //console.log("Hello World");
+//     response.send("Hii");
+// })
+// eslint-disable-next-line no-unused-vars
+app.get("/todos", async (request, response) => {
+  console.log("Todo List");
+  try{
+    const todo = await Todo.findAll();
+    return response.json(todo);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
+});
+
+app.post("/todos", async (request, response) => {
+  console.log("Creating a todo", request.body);
+  try {
+    const todo = await Todo.addTodo({
+      title: request.body.title,
+      dueDate: request.body.dueDate,
+      completed: false,
+    });
+    return response.json(todo);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
+});
+
+//put http://mytodoapp.com/todos/123/markAsCompleted
+app.put("/todos/:id/markAsCompleted", async (request, response) => {
+  console.log("WE have created a todo with ID: ", request.params.id);
+  const todo = await Todo.findByPk(request.params.id);
+  try {
+    const updatedTodo = await todo.markAsCompleted();
+    return response.json(updatedTodo);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
+});
+
+app.delete("/todos/:id", async (request, response) => {
+  console.log("Delete a todo by Id:", request.params.id);
+  const todo = await Todo.findByPk(request.params.id);
+  try {
+    await todo.deleteTodo();
+    // status true indicates that our todo with id is deleted
+    return response.json({ status: true });
+  } catch (error) {
+    // console.log(error);
+    // return response.status(422).json(error);
+    return response.json({response:"The requested ID is not found try again with another ID"});
+  }
+});
+
+module.exports = app;
