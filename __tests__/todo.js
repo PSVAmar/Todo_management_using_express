93996287ec6805b6746ed0bfc1f7eeece9ad794e
PSVAmar <20pa1a05d4@vishnu.edu.in>
2023-03-31 21:21:32 +0530

    tests

---
+++ __tests__/todo.js
@@ -0,0 +1,44 @@
+const request = require("supertest");
+const db = require("../models/index");
+const app = require("../app");
+
+//server be express js application and agent is out http request i.e. used to send the connection requests
+let server, agent;
+
+describe("Todo test suite", () => {
+  beforeAll(async () => {
+    await db.sequelize.sync({ force: true });
+    server = app.listen(3000, () => {});
+    agent = request.agent(server);
+  });
+  afterAll(async () => {
+    await db.sequelize.close();
+    server.close();
+  });
+  test("responds with json at /todos", async () => {
+    const response = await agent.post("/todos").send({
+      title: "Buy Milk",
+      dueDate: new Date().toISOString(),
+      completed: false,
+    });
+    expect(response.statusCode).toBe(302);
+    
+  });
+//   test("Mark a todo as complete", async () => {
+//     const response = await agent.post("/todos").send({
+//       title: "Buy Milk",
+//       dueDate: new Date().toISOString(),
+//       completed: false,
+//     });
+//     const parsedResponse = JSON.parse(response.text);
+//     const todoID = parsedResponse.id;
+
+//     expect(parsedResponse.completed).toBe(false);
+
+//     const markCompleteResponse = await agent
+//       .put(`/todos/${todoID}/markAsCompleted`)
+//       .send();
+//     const parsedUpdateResponse = JSON.parse(markCompleteResponse.text);
+//     expect(parsedUpdateResponse.completed).toBe(true);
+//   });
+});
